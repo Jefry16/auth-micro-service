a750237abe079b61a75959e5961a53a67e077372
Jefry16 <jefrycayo16@gmail.com>
2022-04-24 19:34:27 +0200

    edited headers

--- bootstrap.php
+++ bootstrap.php
@@ -14,15 +14,10 @@ $dotenv->load();
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, DELETE, PATCH');
-    header('Access-Control-Allow-Headers: token, Content-Type');
-    header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
-    die();
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    exit(0);
 }
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PATCH');
-
 header('Content-Type: application/json');
